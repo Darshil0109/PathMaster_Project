f24ec22d90bd0f31a3a5c7466b13e1ef45fff824
Darshil0109 <122811740+Darshil0109@users.noreply.github.com>
2024-03-13 20:42:26 +0530

    Initial_Start
    Start of Floyd Warshell Algorithm

---
+++ Daa_Project.py
@@ -0,0 +1,22 @@
+import numpy as np
+infinity=float('inf')
+a=np.array([[0,8,5],[2,0,infinity],[infinity,1,0]])
+count=0
+temp=[[0,8,5],[2,0,infinity],[infinity,1,0]]
+for w in range(len(a)):
+    if count!=len(a):
+        count+=1
+    for i in range(len(a)):
+        for j in range(len(a)):
+            #print(f"a[{i}][{count-1}] + a[{count-1}][{j}]",a[i][count-1] + a[count-1][j])
+            temp[i][j]=min(a[i][j],(a[i][count-1] + a[count-1][j]))
+            #print(f"temp[{i}][{j}]",temp[i][j])
+        a=temp
+     #print('Temp',temp)    #to see after each iteration change in Given input
+
+#print('a',a)
+
+starting_node=65
+for i in range(len(a)):
+    for j in range(len(a)):
+        print(f"From Node {chr(starting_node+i)} to Node {chr(starting_node+j)} ",a[i][j])
